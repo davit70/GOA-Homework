b60e4c07f4f509a3085cd9ea43c847109e7556bb
davit70 <datotsiqarishvili65@gmail.com>
2026-05-21 19:50:38 +0400

    commit

---
+++ level 2/classwork/cw1.py
@@ -0,0 +1,21 @@
+
+name = 'davit'
+print(name)
+lastname = 'tsikarishvili'
+print(lastname)
+city = 'rustavi'
+print(city)
+car = 'koenigsegg jesko'
+print(car)
+color = 'dark red'
+print(color)
+
+
+print(2 + 2)
+print(9 + 15)
+print(200 - 150)
+print(500 - 390)
+print(90 * 2)
+print(12 * 10)
+print(80 / 4)
+print(9 / 3)
---
+++ level 2/homework/hw1.py
@@ -0,0 +1,60 @@
+# string არის ბრჭყალებში ჩასმული ნებისმიერი სიტყვა
+# integer ში შედის ყველა მთელი რიცხვი (ციფრი)
+# float არის ყველა მთელი რიცხვი
+
+
+
+# ცვლადი არის ფუნქცია რომელშიც ვინახავთ რაიმე სიტყვის მნიშვნელობას მაგალითად color = green
+
+
+
+print('hello world')
+print('davit')
+print('cars')
+print('movies')
+print('colors')
+
+print(90)
+print(100)
+print(123)
+print(999)
+print(15)
+
+
+print(1.2)
+print(5.5)
+print(10.0)
+print(9.10)
+print(5.5)
+
+
+
+
+
+gender = 'male'
+month = 'june'
+color = 'brown'
+brand = 'adidas'
+planet = 'earth'
+
+print(gender)
+print(month)
+print(color)
+print(brand)
+print(planet)
+
+
+
+
+print(9 + 9)
+print(50 + 70)
+print(12 + 9)
+print(3 - 3)
+print(7-80)
+print(8 - 5)
+print(6 * 8)
+print(9 * 260)
+print(60 * 99)
+print(9 / 3)
+print(70 / 2)
+print(100 / 10)
