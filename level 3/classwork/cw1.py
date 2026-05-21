bce7f893697ec6831a4559089e3e5d13c04c4899
davit70 <datotsiqarishvili65@gmail.com>
2026-05-21 23:01:55 +0400

    commit

---
+++ level 3/classwork/cw1.py
@@ -0,0 +1,16 @@
+print(9 + 1)
+print(5 - 4)
+print(10 * 10)
+print(36 / 6)
+print(35 // 8)
+print(25 % 6)
+
+
+
+name = 'davit'
+age = '14 years old'
+color = 'dark red'
+car = 'koenigsegg jesko'
+city = 'rustavi'
+
+print('hello i am ' + name  +  ' i am ' + age  +  ' my favorite color is ' + color  +  ' my favorite car is ' + car  +  ' i live in ' + city)
---
+++ level 3/homework/hw1.py
@@ -0,0 +1,24 @@
+# კოდის წერის პროცესი შედგება სამი ნაწილისგან პირველი არის თავად კოდის დაწერა მეორე ეტაპია მისი შემოწმება თუ რამდენად კარგათ მუშაობს ის ხოლო მესამე არის დებაგი ანუ ხარვეზების პოვნა და მათი აღმოფხვრა
+# / არის ჩვეულებრივი გაყოფა რასაც ყოველდღიურად ვიყენებთ ხოლო // იმ შემთხვევაში გამოიყენება როდესაც გვინდა გავიგოთ რამდენჯერ მოთავსდება ერთი რიცხვი (ციფრი) მეორეში
+# case-sensitive დიდ და პატარა ასოებს სხვადასხვანაირად აღიქვამს ხოლო case-insensitive ყველა ასპოს ერთნაირად იგებს
+
+
+
+intro = ' my name is davit'
+schoolsubject = 'biology'
+subject = 'physics'
+subject1 = 'history'
+subject2 = 'english lessons'
+ 
+print('hello everyone ' + intro + ' i will talk about school subjects ' + schoolsubject + ' is my favorite ' + subject + ' is the one i hate the most ' + subject1 + ' is interesting ' + subject2 + ' has to be the most annoying one')
+
+
+
+
+num1 = 20
+num2 = 10
+num3 = 5
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 / num3 / num2)
