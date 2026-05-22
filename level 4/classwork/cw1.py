c1f462a9e5dcb0ab5e019b010b14546bdf72935a
davit70 <datotsiqarishvili65@gmail.com>
2026-05-22 19:09:32 +0400

    commit

---
+++ level 4/classwork/cw1.py
@@ -0,0 +1,20 @@
+name = input('enter your name ')
+print("your name is " + name)
+
+
+
+my_name = 'davit tsikarishvili'
+my_age = '14'
+city_i__live_in = 'rustavi'
+country = 'Georgia'
+
+print('my name is ' +  my_name  + ' i am ' +  my_age + ' years old ' + ' i live in ' +  country  + ' in a city called ' + city_i__live_in )
+
+
+
+word = 'car'
+num1 = 12
+num2 = 1,2
+print (type(word))
+print (type(num1))
+print (type(num2))
---
+++ level 4/homework/hw1.py
@@ -0,0 +1,63 @@
+# ინფუთი არის ინფორმაცია რომელსაც ჩვენ ვგზავნით მაგალითად როგორც კლავიატურაზე აკრეფილი ასოები თუ ციფრები გადაეცემა კომპიუტერს ხოლო აუთფუთი არის გამოგზავნილი ინფორმაცია ან ასე ვთქვათ სიგნალი მაგალითად როგორც პრინტერს გამოაქვს ფურცელზე დაბეჭილი ინფორმაცია
+
+
+
+# snake_case არის ასე ვთქვათ დამხმარე ხესაწყო რომელიც გვეხმარება ცვლადის სათაურში არა მარტო ერთი არამედ რამდენიმე სიტყვა დაიწეროს ქვედა ტირეს დახმარებით მაგალითად favourite_color = "dark red"
+
+
+
+
+name = input('enter your name')
+print(type (name))
+
+
+# str
+name = 'nika' 
+band = 'gorillaz'
+color = 'black'
+car = 'BMW'
+item = 'pen'
+
+
+# int
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+
+
+# float
+num6 = 5.6
+num7 = 9.1
+num8 = 10.0
+num9 = 12.28
+num10 = 34.3
+
+
+
+
+planet = 'jupiter'
+number = 99
+number2 = 3.2
+print(type(planet))
+print(type(number))
+print(type(number2))
+
+
+
+
+word = 'planet'
+planet2 = 'neptune'
+print( word   +  planet2 )
+
+
+
+
+
+name = 'davit tsikarishvili'
+age = '14 years old '
+height = '165cm '
+weight = '50kg '
+
+print('my name is ' + name + " i am " + age + ' my height is ' + height + ' and i weight ' + weight)
