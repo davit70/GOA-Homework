b60af4871bcf0c2f56ec7bab8dd7fe97a33f8427
davit70 <datotsiqarishvili65@gmail.com>
2026-05-24 01:40:29 +0400

    commit

---
+++ level 6/classwork/cw1.py
@@ -0,0 +1,20 @@
+n1 = int(input('first number '))
+n2 = int(input('second number '))
+
+print(n1 + n2)
+
+
+
+
+age = input('how old are you ')
+print(int(age) + 15 )
+
+
+
+
+print(9 > 19)
+print(12 < 18)
+print(10 == '10')
+print(1 != 3)
+print(7 >= 3)
+print(9 <= 5)
