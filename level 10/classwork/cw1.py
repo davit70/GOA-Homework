38897075851bb985ea99e17b6a58349f8c9a50cc
davit70 <datotsiqarishvili65@gmail.com>
2026-05-26 22:10:24 +0400

    commit

---
+++ level 10/classwork/cw1.py
@@ -0,0 +1,15 @@
+for i in range(0, 11):
+    print(i)
+
+for i in range(0, 102, 2):
+    print(i)
+
+
+
+
+name = input('enter your name ')
+
+if name == 'petre':
+  print('your name is petre')
+else:
+   print('your name is not petre')
