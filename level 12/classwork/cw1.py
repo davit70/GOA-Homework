007d2072b3776266125a1e8bd93c6c3cd083d998
davit70 <datotsiqarishvili65@gmail.com>
2026-05-29 21:23:24 +0400

    commit

---
+++ level 12/classwork/cw1.py
@@ -0,0 +1,16 @@
+weather = input('whats the weather outside ')
+if weather == 'sunny':
+  print('i will workout outside')
+elif weather == 'cloudy':
+  print('i will workout outside later')
+else:
+  print('i wont workout today')
+
+
+
+
+for i in range(1, 1000):
+ print(i)
+ if i == 461:
+  print(i)
+  break
