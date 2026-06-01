928732183a1e1e66e1ab22fbd0ce01f33416a1a0
davit70 <datotsiqarishvili65@gmail.com>
2026-06-01 17:42:40 +0400

    commit

---
+++ hw7.py
@@ -0,0 +1,38 @@
+# number2 = 20
+# while number2 <= 50:
+#     print(number2)
+#     number2 = number2 + 1
+
+
+
+# n = float(input('enter any number '))
+# n2 = float(input('enter any number '))
+# n3 = float(input('enter any number '))
+# if n >= n2 and n >= n3:
+#     print(n)
+# elif n2 >= n and n2 >= n3:
+#     print(n2)
+# else:
+#     print(n3)
+
+
+
+# number6 = int(input("enter number "))
+# sum = 0
+# for i in range(1, number6, +1):
+#     sum = sum +i
+# print(sum)
+
+
+
+# for i in range(1, 301):
+#     if i %4 == 0 and i %7 == 0:
+#         print(i)
+#         break
+
+
+
+for i in range(1, 51):
+    if i %10 == 0:
+        continue
+    print(i)
