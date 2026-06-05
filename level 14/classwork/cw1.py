35ddd43c644c6f20d53b03cf5cf3c42316022978
davit70 <datotsiqarishvili65@gmail.com>
2026-06-05 20:59:29 +0400

    commit

---
+++ level 14/classwork/cw1.py
@@ -0,0 +1,8 @@
+# 1) შექმენით 5 ელემენტიანი სია, სადაც ჩაწერთ თქვენს საყვარელი მანქანების სახელს. ბოლოს გამოიტანეთ სიაში მყოფი პირველი და მესამე მნიშვნელობა. 
+# (დადებითი index-ებით)
+
+
+cars = ['dodge charger', 'dodge challemger', 'koenigsegg jesko', 'toyota supra mk4', 'nissan skyline']
+print(cars[0])
+print(cars[2])
+
