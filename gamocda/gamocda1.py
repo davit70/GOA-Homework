5c8b312304bff0f0bff1e02dc9231b57cb344b0b
davit70 <datotsiqarishvili65@gmail.com>
2026-06-24 20:49:03 +0400

    commit

---
+++ gamocda/gamocda1.py
@@ -0,0 +1,26 @@
+# 3)გააკეთეთ  3-3 მაგლითი .upper(), .lower(), .capitalize() ის გამოყენებით
+
+
+print('dog'.upper())
+print('cat'.upper())
+print('computer'.upper())
+
+print('NUMBERS'.lower())
+print('ONLINE'.lower())
+print('GEORGIA'.lower())
+
+
+print('names'.capitalize())
+print('davit'.capitalize())
+print('germany'.capitalize())
+
+
+
+
+# 5)შექმენით 7 ელემენტიანი სახელების სია, შეცვალეთ 1-ლი და მე-5 ინდექსზე მდგომი ელემენტების მნიშვნელობა
+
+names=['davit', 'nika', 'luka', 'lasha', 'mari', 'ilia', 'lile']
+
+names[1]='ilia'
+names[5]='nika'
+print(names)
