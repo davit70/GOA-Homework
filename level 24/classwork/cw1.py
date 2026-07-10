358ace7bfaaaf9e29e847a27f45c00f6459a85e2
davit70 <datotsiqarishvili65@gmail.com>
2026-07-10 16:48:58 +0400

    commit

---
+++ level 24/classwork/cw1.py
@@ -0,0 +1,8 @@
+# 1) შექმენით ფუნქცია სახელად greet, რომელიც დაბეჭდავს მისალმების ტექსტებს. "Hello World!" და "Hello {name}".
+
+
+def greet(name):
+    print('hello world')
+    print(f"hello {name}")
+
+greet('Davit')
