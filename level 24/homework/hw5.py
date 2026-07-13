b13311e8a890867e3c8b07a38cb5cb3b6662d72c
davit70 <datotsiqarishvili65@gmail.com>
2026-07-13 17:04:22 +0400

    commit

---
+++ level 24/homework/hw5.py
@@ -0,0 +1,5 @@
+# 6) შექმენით ფუნქცია, რომელიც გადაცემულ ტექსტს აბრუნებს დიდი ასოებით.
+
+def text(txt):
+    return txt.capitalize()
+print(text('davit'))
---
+++ level 24/homework/hw7.py
@@ -0,0 +1,7 @@
+# 7) შექმენით ფუნქცია, რომელსაც პარამეტრებად გადაეცემა სახელი და გვარი, 
+# თქვენი დავალებაა გამოიტანოთ მნიშვნელობები ერთი წინადადების სახით.
+
+
+def fullname(name, lastname):
+    return name + lastname
+print(fullname('davit ', ' tsikarishvili'))
