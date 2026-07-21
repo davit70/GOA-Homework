d8c15ebb409a7b48992fe234cf2d749ab0468985
davit70 <datotsiqarishvili65@gmail.com>
2026-07-21 17:18:52 +0400

    commit

---
+++ gamocda/gamocda2/gamocda4.py
@@ -0,0 +1,11 @@
+# 4) გააკეთეთ თითო მაგალითი .upper(); .lower(); .capitalize(); .find(), .count(), len(), .endswith(), .startswith() ებზე
+print('davit'.upper())
+print('mouse'.lower())
+print('germany'.capitalize())
+colors = ['green', 'purple','red', 'green', 'blue']
+color = 'red green blue'
+print(color.find('g'))
+print(colors.count('green'))
+print(len(color))
+print(color.endswith('blue'))
+print(color.startswith('green'))
